be9c5d28c7c057580b41a4101734066c59bdf333
SHjNEe <baotrung.jpvn@gmail.com>
2023-11-18 16:13:56 +0700

    Add

---
+++ .github/actions/deploys3action/main.js
@@ -0,0 +1,8 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+
+async function run() {
+  core.notice("Github acitons from S3 deploy");
+}
+run();
