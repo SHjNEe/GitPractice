5044eba57c94029fd4bc9e5e9a62bf85e0af76d9
SHjNEe <baotrung.jpvn@gmail.com>
2023-11-18 16:56:27 +0700

    Deploy to s3

--- .github/actions/deploys3action/main.js
+++ .github/actions/deploys3action/main.js
@@ -1,8 +1,43 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
+// const AWS = require("aws-sdk");
+// const fs = require("fs");
 
 async function run() {
-  core.notice("Github acitons from S3 deploy");
+  try {
+    // Get input values from GitHub Actions inputs
+    const bucketName = core.getInput("bucket-name", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+    //Upload files
+    const s3Uri = `s3:://${bucketName}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    // // Load AWS credentials and configure S3
+    // const s3 = new AWS.S3({
+    //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    //   region: bucketRegion,
+    // });
+
+    // // Read the file to upload
+    // const fileContent = fs.readFileSync(filePath);
+
+    // // Define params for S3 upload
+    // const params = {
+    //   Bucket: bucketName,
+    //   Key: s3ObjectName,
+    //   Body: fileContent,
+    // };
+
+    // // Upload file to S3
+    // await s3.upload(params).promise();
+
+    core.info(`File uploaded successfully to S3: ${bucketName}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
 run();
